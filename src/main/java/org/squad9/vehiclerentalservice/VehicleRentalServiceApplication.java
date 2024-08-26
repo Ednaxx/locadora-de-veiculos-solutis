0afaee3ecb7ff6ae06e4f22a2797f525f6095824
Alexandre Morais <xandre2setembro2002@hotmail.com>
2024-08-26 09:33:29 -0300

    feat: Project setup

---
+++ src/main/java/org/squad9/vehiclerentalservice/VehicleRentalServiceApplication.java
@@ -0,0 +1,13 @@
+package org.squad9.vehiclerentalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleRentalServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/squad9/vehiclerentalservice/VehicleRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.squad9.vehiclerentalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
